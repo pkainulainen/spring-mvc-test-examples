8da617055850cf7f22e21f5c42165a783792fb48
Petri Kainulainen <petri.kainulainen@gmail.com>
2013-05-16 23:20:59 +0300

    restructured imports

--- rest-jsonpath/src/integration-test/java/net/petrikainulainen/spring/testmvc/todo/controller/ITTodoControllerTest.java
+++ rest-jsonpath/src/integration-test/java/net/petrikainulainen/spring/testmvc/todo/controller/ITTodoControllerTest.java
@@ -33,7 +33,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
